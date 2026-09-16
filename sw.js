aab70bb497b8daec6dcc14207565c1ef7e596fe0
Dika-nurut <Dika-nurut@users.noreply.github.com>
2026-09-16 16:59:21 +0300

    Redesign v2: Duolingo-derived tokens, 3D buttons, footer keypad, real type scale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cooprep-v2';
+const CACHE = 'cooprep-v3';
 const ASSETS = ['./', './index.html', './bank.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
