0257918e23ad5f6e79b40fc2e6acd9f256577c7e
Dika-nurut <Dika-nurut@users.noreply.github.com>
2026-09-16 23:01:37 +0300

    Finish: risk register table, project schedule table, negotiation and motivation judgment tasks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cooprep-v7';
+const CACHE = 'cooprep-v8';
 const ASSETS = ['./', './index.html', './bank.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
