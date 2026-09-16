f05591762bdccae5b95945ec59f09ddeeef10c9d
Dika-nurut <Dika-nurut@users.noreply.github.com>
2026-09-16 18:55:54 +0300

    Step 4: add PM craft module (metric decomposition, estimation, risk, throughput, data tables, negotiation, motivation) + CPI/VAC

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cooprep-v5';
+const CACHE = 'cooprep-v6';
 const ASSETS = ['./', './index.html', './bank.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
