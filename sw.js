d769dbcad0da4f0c3d2d5177c9e0cd042ced06fd
Diyor <mr.dika.98@gmail.com>
2026-09-18 15:25:18 +0300

    Teach combinations from pairs to factorials

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cooprep-v19';
-const ASSETS = ['./', './index.html', './bank.js?v=1.9', './expansion.js?v=1.5', './knowledge.js?v=1.4', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'cooprep-v20';
+const ASSETS = ['./', './index.html', './bank.js?v=2.0', './expansion.js?v=1.5', './knowledge.js?v=1.5', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
